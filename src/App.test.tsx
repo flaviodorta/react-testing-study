2cd22c8fc3a952e7fbbfe833dbccd3b7c32bdc0b
flaviodorta <dorta.dev@gmail.com>
2022-09-12 16:09:54 -0300

    first commit

---
+++ src/App.test.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import { render, screen } from '@testing-library/react';
+import App from './App';
+
+test('renders learn react link', () => {
+  render(<App />);
+
+  const coloredButton = screen.getByRole('button', { name: 'Change a color' });
+
+  expect(coloredButton).toHaveStyle({
+    backgroundColor: 'red',
+  });
+});
---
+++ src/App.tsx
@@ -0,0 +1,9 @@
+export default function App() {
+  return (
+    <div className='flex items-center justify-center w-screen h-screen bg-gradient-to-r from-indigo-500 via-purple-500 to-pink-500'>
+      <button className='bg-[red] active:bg-red-700 rounded-lg p-3'>
+        Change a color
+      </button>
+    </div>
+  );
+}
